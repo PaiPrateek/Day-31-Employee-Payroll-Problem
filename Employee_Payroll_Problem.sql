b6bf91ff1f7e5e0c37ff8e17ab30029b49a4d28a
Prateek Pai <paiprateek123@gmail.com>
2022-07-18 22:26:41 +0530

    Insert data in to employee_payroll table in payroll_service database

--- Employee_Payroll_Problem.sql
+++ Employee_Payroll_Problem.sql
@@ -15,3 +15,6 @@ startdate date not null)
 
 --to select all / display the table
 select * from employee_payroll
+
+--Inserting the data in to table
+INSERT INTO employee_payroll values ('Prateek',25000,'2020-02-14'),('Manjesh',20000,'2019-02-10'),('Suhas',35000,'2019-11-04'),('Prajwal',15000,'2020-06-20'),('Sanath',18000,'2018-08-01');
