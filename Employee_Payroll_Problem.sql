0f8cc5f92137fa993ed69fd979956be7124696ae
Prateek Pai <paiprateek123@gmail.com>
2022-07-18 22:19:46 +0530

    Creating the employee_payroll table in payroll_service database

--- Employee_Payroll_Problem.sql
+++ Employee_Payroll_Problem.sql
@@ -1,6 +1,17 @@
 --Welcome to Employee Payroll Problem 
+
+--Creating the database
 Create Database payroll_service
 
 --go to the databse
 use payroll_service
 
+--Creating the Table
+Create Table employee_payroll(
+id int primary key identity not null,
+name varchar(20) not null,
+salary int not null,
+startdate date not null)
+
+--to select all / display the table
+select * from employee_payroll
